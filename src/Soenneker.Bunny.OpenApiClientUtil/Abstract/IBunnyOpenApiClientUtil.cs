606fadfcadee5361802db8419264f5bfdce52b26
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:11 -0500

    xml commenting

--- src/Soenneker.Bunny.OpenApiClientUtil/Abstract/IBunnyOpenApiClientUtil.cs
+++ src/Soenneker.Bunny.OpenApiClientUtil/Abstract/IBunnyOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Bunny.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IBunnyOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<BunnyOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
